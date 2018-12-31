585f816ea916c2098b83b8046f6dee378b4aca65
Minjae Lee <minjae@live.com>
2018-12-31 21:14:00 +0900

    Value Serializer 변경

--- src/main/java/com/example/springdataredis/config/RedisConfig.java
+++ src/main/java/com/example/springdataredis/config/RedisConfig.java
@@ -1,8 +1,5 @@
 package com.example.springdataredis.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import static io.lettuce.core.ReadFrom.SLAVE_PREFERRED;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +11,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceClientConfigurat
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.SerializationException;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -24,7 +19,7 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         // master-slave 설정, 쓰기는 마스터에서, 읽기는 슬레이브에서..
-        // TODO: 더 높은 가용성을 위새 sentinel 사용
+        // TODO: 더 높은 가용성을 위해 sentinel 사용
         RedisConfiguration redisConfiguration = new RedisStandaloneConfiguration();
 
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
@@ -43,42 +38,8 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(rcf);
         template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(new JsonRedisSerializer());
         template.setValueSerializer(new Jackson2JsonRedisSerializer(Object.class));
 
         return template;
-    }
-
-    class JsonRedisSerializer implements RedisSerializer<Object> {
-
-        private final ObjectMapper om;
-
-        public JsonRedisSerializer() {
-            this.om = new ObjectMapper()
-                    .enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-        }
-
-        @Override
-        public byte[] serialize(Object t) throws SerializationException {
-            try {
-                return om.writeValueAsBytes(t);
-            } catch (JsonProcessingException e) {
-                throw new SerializationException(e.getMessage(), e);
-            }
-        }
-
-        @Override
-        public Object deserialize(byte[] bytes) throws SerializationException {
-
-            if (bytes == null) {
-                return null;
-            }
-
-            try {
-                return om.readValue(bytes, Object.class);
-            } catch (Exception e) {
-                throw new SerializationException(e.getMessage(), e);
-            }
-        }
     }
 }
